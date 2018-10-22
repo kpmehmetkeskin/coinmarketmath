20a00b40f4fc507fe6962415ef3271c246826c82
kpmehmetkeskin <mehmet.keskin@koctas.com.tr>
2018-10-22 13:56:51 +0300

    Splash screen in bir kez açılması sağlandı.

--- app/src/main/java/com/futuremesalabs/coinmarketmath/SplashActivity.java
+++ app/src/main/java/com/futuremesalabs/coinmarketmath/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.futuremesalabs.coinmarketmath;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,11 +12,17 @@ public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        SharedPreferences prefs = getSharedPreferences("A_VARIABLE_781411", MODE_PRIVATE);
+        Boolean igotit = prefs.getBoolean("igotit", false);
+        if (igotit != null && igotit.equals(true)) {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
 
         setTitle("User Guide");
         Toast.makeText(getApplicationContext(), "Tap to pass", Toast.LENGTH_SHORT).show();
@@ -23,6 +30,9 @@ public class SplashActivity extends AppCompatActivity {
         final ImageView imageView = findViewById(R.id.splashImageView);
         imageView.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                SharedPreferences.Editor editor = getSharedPreferences("A_VARIABLE_781411", MODE_PRIVATE).edit();
+                editor.putBoolean("igotit", true);
+                editor.apply();
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
             }
